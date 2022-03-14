7aba955857112251219b01d1442b9090f631c2a2
Agathe1509 <101548491+Agathe1509@users.noreply.github.com>
2022-03-14 09:54:11 +0100

    Update plateau.java

--- plateau.java
+++ plateau.java
@@ -3,26 +3,40 @@ import java.util.*;
 public class plateau{
 	
 	
-	public int[][] plateau;
+	public int[][] jeu;
 	
 	public plateau (LinkedList<Pair>list){
-		int n = Math.sqrt((Pair.size())*2);	/pas de problem car les listes sont composées d'un nombre de paire de telle sorte à obtenir une matrice carrée
-		plateau = new int [n][n];
 		
-		p=0;
-		While(!Pair.isEmpty()){
-			int i = (int)(Math.random()*n);	/renvoie un indice de position entre 0 et n
+		//pas de problem car les listes sont composées d'un nombre de paire de telle sorte à obtenir une matrice carrée
+		int n = (int)(Math.sqrt((list.size())*2));	
+		jeu = new int [n][n];
+		
+		
+		for(Pair p : list){
+			//indice des positions des nombres de la pair
+			int i = (int)(Math.random()*n);	
 			int j = (int)(Math.random()*n);
 			int k = (int)(Math.random()*n);
 			int l = (int)(Math.random()*n);
-			
-			if (plateau[i][j] == null && plateau[k][l] == null && (i!=k || j!=l){	/on vérifie qu'il n'y est pas déjà un chiffre aux emplacements (i,j),(k,l)
-				plateau[i][j] == Pair.get(p.get(a));
-				plateau[k][l] == Pair.get(p.get(b));
-				p++;			
-			}
-	
-		}
+			if(estVide(i,j)){
+				jeu[i][j] = p.a;
+			}else{
+				
+			}	
+			if(estVide(k,l)){
+				jeu[k][l] = p.b;
+			}else{
+				
+			}	
+		}	
 	}
+	
+	public boolean estVide (int i, int j){
+		boolean vide = false;
+		if(jeu[i][j] == 0){
+			vide = true;
+		}
+		return vide;
+	}	
 	
 }
