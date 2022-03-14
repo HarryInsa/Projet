8b323df519c694a5fc87aa60db4d332f78451d7a
HarryInsa <101262737+HarryInsa@users.noreply.github.com>
2022-03-14 08:56:11 +0100

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,13 +1,24 @@
 public class Pair {
     
+    private int a ;
+    private int b ;
     
     
-    public Pair {
-            
-            
-            
-            
-            
-            
+    public Pair(int nb1, int nb2) {
+        this.a = nb1;
+        this.b = nb2;
+    }
+    
+    //Méthode de comparaison renvoie true ou false
+    public boolean equals(Pair p1){
+        if(this.a==p1.a && this.b==p1.b || this.b==p1.a && this.a==p1.b)
+            return true;
+        else
+            return false;
+    }
+    
+    // Affichage
+    public String toString(){
+        return "("+a+","+b+")";
     }
 }
