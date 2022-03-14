b7559f212e8ee0c620274bc2aa9db3c474f9abc6
Agathe1509 <101548491+Agathe1509@users.noreply.github.com>
2022-03-14 17:26:16 +0100

    Update partie.java

--- partie.java
+++ partie.java
@@ -1,1 +1,49 @@
+import java.util.* ;
+
 public class partie {
+	
+	int nombre;
+	int nombrePair;
+	LinkedList <Pair> list;
+	plateau jeu;
+	
+	public partie (int niveau){
+		
+		creationNiveau(niveau);
+		
+		//Création du nombre clé entre 50 et 100
+		nombre = (int)(Math.random()*50+50);
+		
+		//Création de la list contenant toutes les pairs pour le jeu
+		list = new LinkedList <Pair> ();
+		while (list.size()<nombrePair){
+			list.add(creationPair());
+		}
+		
+		//Création du plateau de jeu
+		jeu = new plateau (list);
+		
+	}
+	
+	//Donne le nombre de pair à créer selon le niveau choisit
+	public void creationNiveau (int niveau){
+		if (niveau == 1){
+			nombrePair = 8;
+		}else if (niveau == 2){
+			nombrePair = 18;
+		}else{
+			nombrePair = 32;
+		}
+	}
+	
+	//Crétion d'une pair aléatoire telle que a + b = nombre
+	public Pair creationPair (){
+		int a = (int)(Math.random()*50+50);
+		while(a>nombre){
+			a = (int)(Math.random()*50+50);
+		}
+		int b = (int)(nombre - a);
+		Pair pair = new Pair (a, b);
+		return pair;
+	}
+}
