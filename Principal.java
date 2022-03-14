9cb10aac8d902efa9974b17b3206f99b969b70f2
Agathe1509 <101548491+Agathe1509@users.noreply.github.com>
2022-03-14 09:55:04 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,9 @@
 public class Principal {
     
     public static void main (String [] args){
-        
+		
+		partie jeu = new partie (1);
+		FenetrePrincipale fenetre = new FenetrePrincipale(jeu.plateau);
+		
     }
 }
