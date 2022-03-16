565339916323780fef3a74ff38cd27789b080f54
HarryInsa <101262737+HarryInsa@users.noreply.github.com>
2022-03-16 16:06:19 +0100

    Update FenetreNiveau.java

--- FenetreNiveau.java
+++ FenetreNiveau.java
@@ -3,36 +3,58 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.* ;
 
-public class FenetreNiveau extends JFrame {
-    
+public class FenetreNiveau extends JFrame implements ActionListener {
+    int niveau;
     JButton bouton1;
     JButton bouton2;
     JButton bouton3;
+    //Image sky;
     
     public FenetreNiveau(){
-        super("ChoixNiveau");
+        super("Menu");
         setSize(400,400);
 		setLocation(100,200);
+        //sky = Toolkit.getDefaultToolkit().getImage("IMG_0242.JPG");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
         
         //instanciation boutons
         bouton1 = new JButton("Niveau 1");
-        bouton1.setBounds(0,10,80,40);
+        bouton1.setBounds(65,130,80,40);
+        bouton1.addActionListener(this);
         bouton2 = new JButton("Niveau 2");
-        bouton2.setBounds(90,10,80,40);
+        bouton2.setBounds(155,130,80,40);
+        bouton2.addActionListener(this);
         bouton3 = new JButton("Niveau 3");
-        bouton3.setBounds(180,10,80,40);
+        bouton3.setBounds(245,130,80,40);
+        bouton3.addActionListener(this);
         
-        //rangement
+        //mettre un champ texte
+                
+        //rangement et voir pourquoi contenuer général marche pas
         JPanel monConteneur = new JPanel();
         monConteneur.setLayout(null);
         monConteneur.add(bouton1);
 		monConteneur.add(bouton2);
 		monConteneur.add(bouton3);
-		monConteneur.setBounds(100,200,160,40);
+		monConteneur.setBounds(0,0,0,0);
 		add(monConteneur);
+        
+ 
         
         setVisible(true);
       	
+    }
+    public void actionPerformed (ActionEvent e){
+       if (e.getSource() == bouton1){
+            niveau=1;
+            System.exit(0) ;
+        }else if (e.getSource()== bouton2){
+            niveau = 2;
+            System.exit(0) ;
+        }else{
+            niveau = 3;
+            System.exit(0) ;
+        }
     }
 }
