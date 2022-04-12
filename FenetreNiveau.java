7f218dc8d8dc6a36be1631944dc4024a27e4190f
Agathe1509 <101548491+Agathe1509@users.noreply.github.com>
2022-04-12 13:22:24 +0200

    Update FenetreNiveau.java

--- FenetreNiveau.java
+++ FenetreNiveau.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.* ;
 import javax.swing.UIManager;
-import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.*;
@@ -12,38 +12,26 @@ import java.util.*;
 public class FenetreNiveau extends JFrame implements ActionListener{
     
     //Atributs
-    public int niveau = 0;
-    public int operation = 0;
-    public JButton bouton1;
-    public JButton bouton2;
-    public JButton bouton3;
-    public JButton boutonadd;
-    public JButton boutonsous;
-    public JButton quitter;
-    public JButton boutonJouer;
-    public Font police;
-    public Font police2;
-    public Font police3;
-    public String res;
-    public String res1;
-    public String res3;
+    public Font policeTaille14, policeTaille16, policeTaille18;
     public JMenu menu;
     public JMenuBar menubar;
-    public JMenuItem regle;
-    public JMenuItem autre;
-    public JLabel chpsNiveau;
-    public JLabel chpsOpe;
-    public JLabel horloge;
-    public ImageIcon icon;
+    public JMenuItem regle, autre;
+    public String reglejeu, inforamtion, erreur;
+    public JButton bouton1, bouton2, bouton3, boutonadd, boutonsous, quitter, boutonJouer, son;
+	public JLabel chpsNiveau, chpsOpe, horloge;
+	public int niveau = 0, operation = 0;
     public partie p;
     public FenetrePrincipale f;
-    public boolean click1 = false;
-    public boolean click2 = false;
-    public boolean jouer = false;
+    public boolean click1 = false, click2 = false, jouer = false, b;
+    public ImageIcon iconSonOn, iconSonOff, icon;
+    public Image imageSonOn, imageSonOff;
+    public Sound leSon;
     
     
-    
-    public FenetreNiveau(){
+    public FenetreNiveau(Sound leSon, boolean b){
+        
+        this.b = b;
+        this.leSon = leSon;
         
         //Définition de la fenetre
         this.setTitle("Bienvenue - The Memory");
@@ -51,11 +39,12 @@ public class FenetreNiveau extends JFrame implements ActionListener{
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setIconImage(new ImageIcon(FenetreAccueil.class.getResource("M.png")).getImage());
         
         //définition des polices d'écriture
-        police3 = new Font(" Arial " , Font.PLAIN , 12);
-        police = new Font(" Arial " , Font.PLAIN , 16);
-        police2 = new Font(" Arial " , Font.PLAIN , 13);
+        policeTaille14 = new Font("Cambria", Font.PLAIN , 14);
+        policeTaille16 = new Font("Cambria", Font.PLAIN , 16);
+        policeTaille18 = new Font("Cambria", Font.PLAIN , 18);
         
         //Initialisation du niveau à 0
         niveau = 0;
@@ -66,100 +55,122 @@ public class FenetreNiveau extends JFrame implements ActionListener{
         
         //Boutons de choix niveau
         bouton1 = new JButton("Niveau 1");
-        bouton1.setFont(police);
+        bouton1.setFont(policeTaille16);
         bouton1.setBounds(25,180,100,50);
         bouton1.addActionListener(this);
         monConteneur.add(bouton1);
         
         bouton2 = new JButton("Niveau 2");
-        bouton2.setFont(police);
+        bouton2.setFont(policeTaille16);
         bouton2.setBounds(150,180,100,50);
         bouton2.addActionListener(this);
         monConteneur.add(bouton2);
         
         bouton3 = new JButton("Niveau 3");
         bouton3.setBounds(275,180,100,50);
-        bouton3.setFont(police);
+        bouton3.setFont(policeTaille16);
         bouton3.addActionListener(this);
         monConteneur.add(bouton3);
         
         //Boutons choix opération
         boutonadd = new JButton("Addition");
         boutonadd.setBounds(25, 50, 150, 50);
-        boutonadd.setFont(police);
+        boutonadd.setFont(policeTaille18);
         boutonadd.addActionListener(this);
         monConteneur.add(boutonadd);
         
         boutonsous = new JButton("Soustraction");
         boutonsous.setBounds(225, 50, 150, 50);
-        boutonsous.setFont(police);
+        boutonsous.setFont(policeTaille18);
         boutonsous.addActionListener(this);
         monConteneur.add(boutonsous);
         
 		//Bouton quitter
         quitter = new JButton("Quitter");
-        quitter.setBounds(0,355,100,50);
-        quitter.setFont(police3);
+        quitter.setBounds(10,355,100,50);
+        quitter.setFont(policeTaille16);
         quitter.setForeground(Color.RED);
         quitter.addActionListener(this);
         monConteneur.add(quitter);
         
         //Bouton Jouer
         boutonJouer = new JButton("Jouer !");
-        boutonJouer.setBounds(320,355,100,50);
-        boutonJouer.setFont(police3);
+        boutonJouer.setBounds(305,355,100,50);
+        boutonJouer.setFont(policeTaille18);
         boutonJouer.addActionListener(this);
         monConteneur.add(boutonJouer);
         
+        //Bouton son
+		iconSonOn = new ImageIcon("./SonOn.png");
+        imageSonOn = scaleImage(iconSonOn.getImage(), 30, 30);
+        
+        iconSonOff = new ImageIcon("./SonOff.png");
+        imageSonOff = scaleImage(iconSonOff.getImage(), 30, 30);
+        
+        son = new JButton();
+        son.setBounds(385,0, 30, 30);
+        son.setBackground(Color.WHITE);
+        
+        if(!b){
+			son.setIcon(new ImageIcon(imageSonOn));
+        }else{
+			son.setIcon(new ImageIcon(imageSonOff));
+		}	
+        
+        son.addActionListener(this);
+        monConteneur.add(son);
+       
+        
         //Affichage niveau choisit
         chpsNiveau = new JLabel();
-        chpsNiveau.setFont(police);
+        chpsNiveau.setFont(policeTaille18);
         chpsNiveau.setForeground(Color.WHITE);
         chpsNiveau.setBounds(222,230,50,50);
         monConteneur.add(chpsNiveau);
         
         //Affichage opération choisit
         chpsOpe = new JLabel();
-        chpsOpe.setFont(police);
+        chpsOpe.setFont(policeTaille18);
         chpsOpe.setForeground(Color.WHITE);
-        chpsOpe.setBounds(233,110,140,50);
+        chpsOpe.setBounds(243,110,140,50);
         monConteneur.add(chpsOpe);
         
         //Etiquette info niveau
         JLabel chpsNiveau = new JLabel("Vous avez choisit le niveau :");
         chpsNiveau.setBounds(25,230,200,50);
         chpsNiveau.setForeground(Color.WHITE);
-        chpsNiveau.setFont(police2);
+        chpsNiveau.setFont(policeTaille16);
         monConteneur.add(chpsNiveau);
         
         //Etiquette info operation
         JLabel chpsope = new JLabel("Vous avez choisit l'opération :");
-        chpsope.setBounds(25, 110, 200, 50);
+        chpsope.setBounds(25, 110, 210, 50);
         chpsope.setForeground(Color.WHITE);
-        chpsope.setFont(police2);
+        chpsope.setFont(policeTaille16);
         monConteneur.add(chpsope);
         
-        //Etiquette date et heure
+        //Etiquette date
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 		Date date = new Date();
 		horloge = new JLabel(format.format(date));
 		horloge.setForeground(Color.WHITE);
-		horloge.setBounds(25,-30, 400, 100);
+		horloge.setBounds(25,-20, 400, 100);
+		horloge.setFont(policeTaille16);
         monConteneur.add(horloge);
        
         //Création du menu
         menu = new JMenu("Menu");
-        menu.setFont(police);
+        menu.setFont(policeTaille18);
         menubar = new JMenuBar();
         
         //Items du menu et leurs caractéristiques
         regle = new JMenuItem("Règles du jeu");
         regle.addActionListener(this);
-        regle.setFont(police);
+        regle.setFont(policeTaille18);
         
-        autre = new JMenuItem("Autres");
+        autre = new JMenuItem("Informations");
         autre.addActionListener(this);
-        autre.setFont(police);
+        autre.setFont(policeTaille18);
         
         menu.add(regle);
         menu.add(autre);
@@ -168,15 +179,11 @@ public class FenetreNiveau extends JFrame implements ActionListener{
         this.setJMenuBar(menubar);
 		
         //Texte de présentation du menu
-        res = "Ce jeu est un memory. \nL'ordinateur choisit un nombre entre 50 et 100.";
-        res += "\nVotre but est de retrouver toutes les paires dont la somme fait le nombre choisit !";
-        res += "\nCe jeu vous entraine donc à la fois votre mémoire et développe aussi votre capacité de calcul mental :)";
-        res1= "- Jeu réalisé par Agathe Vincent, Harry Kalfon et Noa Portier \n-2022 - Projet Informatique - Insa LYON -";
-        res3="Veuillez d'abord choisir un type et un niveau de jeu !";
-        
-        //pour changer la couleur des joption
-		// UIManager.put("OptionPane.background",new ColorUIResource(54,66,230));
-        //UIManager.put("Panel.background",new ColorUIResource(54,66,230));        
+        reglejeu = "Ce jeu est un memory où il faut former des pairs en faisant au choix des sommes ou des soustractions. \nL'ordinateur choisit un nombre entre 10 et 70 selon le niveau choisit.";
+        reglejeu += "\nLe nombre de pairs à retrouver dépend du niveau choisit !";
+        reglejeu += "\nLe but du jeu est de retrouver toutes les pairs. Ce jeu vous entraine donc à la fois votre mémoire et développe aussi votre capacité de calcul mental :)";
+        inforamtion = "- Jeu réalisé par Agathe Vincent, Harry Kalfon et Noa Portier - \n- 2022 - Projet Informatique - Insa LYON -";
+        erreur = "Veuillez d'abord choisir un type et un niveau de jeu !";
         
         //Fond d'écran - image        
         icon = new ImageIcon("./LogoPrinc.png");
@@ -198,11 +205,13 @@ public class FenetreNiveau extends JFrame implements ActionListener{
             niveau = 1;
 			click1 = true;
         }
+        
         if (e.getSource() == bouton2){
             chpsNiveau.setText("2");
             niveau = 2;
 			click1 = true;
         }
+        
         if (e.getSource() == bouton3){
 			chpsNiveau.setText("3");
 			niveau = 3;
@@ -214,29 +223,49 @@ public class FenetreNiveau extends JFrame implements ActionListener{
 			operation = 1;	//1 pour le choix de l'addition
 			click2 = true;
 		}
+		
         if (e.getSource() == boutonsous){
 			chpsOpe.setText("Soustraction");
 			operation = 2;	//2 pour le choix de la soustraction
 			click2 = true;
-		}	
+		}
+			
         if(e.getSource() == boutonJouer){
-            if(click1==false || click2==false){
-                JOptionPane.showMessageDialog(this,res3,"Attention",1,null); //si la personne clique sur jouer sans avoir remplit les conditions
-                jouer=false;
+            if(click1 == false || click2 == false){
+                JOptionPane.showMessageDialog(this, erreur,"Attention",1,null); //si la personne clique sur jouer sans avoir remplit les conditions
+                jouer = false;
             }else{
-            jouer=true;
+				jouer = true;
+				chpsOpe.setText("");
+				chpsNiveau.setText("");
             }
         }
-        if (e.getSource()==regle){
-            JOptionPane.showMessageDialog(this,res,"Règles du Jeu",1,null); 
+        
+        if (e.getSource() == regle){
+            ImageIcon iconjeu = new ImageIcon("jeu.png");
+            Image imagejeu = scaleImage(iconjeu.getImage(), 30,30);
+            UIManager.put("OptionPane.background", new ColorUIResource(255, 255, 255));
+			UIManager.put("Panel.background", new ColorUIResource(255, 255, 255));
+			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font ("Cambria", Font.PLAIN , 14)));
+            JOptionPane.showMessageDialog(this, reglejeu, "Règles du jeu", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(imagejeu));
         }
-        if(e.getSource()==autre){
-            JOptionPane.showMessageDialog(this,res1,"Informations",1,null);
+        
+        if(e.getSource() == autre){
+            ImageIcon lum = new ImageIcon("lumière.jpg");
+            Image lum2 = scaleImage(lum.getImage(), 30,30);
+            UIManager.put("OptionPane.background", new ColorUIResource(255, 255, 255));
+			UIManager.put("Panel.background", new ColorUIResource(255, 255, 255));
+			UIManager.put("OptionPane.messageFont", new FontUIResource(new Font ("Cambria", Font.PLAIN , 14)));
+            JOptionPane.showMessageDialog(this, inforamtion, "Information", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(lum2));
         }
         
         if(e.getSource() == quitter){
             System.exit(0);
         }
+        
+        if(e.getSource() == son){
+			sonOnOff(b);
+		}
         
     }
     
@@ -252,5 +281,18 @@ public class FenetreNiveau extends JFrame implements ActionListener{
 			return false;
 		}	
 	}	
+    
+    //Arreter ou remmetre la musique
+    public void sonOnOff (boolean b){
+		if(b){
+			leSon.jouerEnBoucle();
+			son.setIcon(new ImageIcon(imageSonOn));
+			this.b = false;
+		}else{
+			leSon.arreter();
+			son.setIcon(new ImageIcon(imageSonOff));
+			this.b = true;
+		}		
+	}		
     
 }
