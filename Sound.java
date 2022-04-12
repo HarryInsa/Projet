d37df41f04b331464a022c06737b7f829c1a2307
Agathe1509 <101548491+Agathe1509@users.noreply.github.com>
2022-04-12 13:24:55 +0200

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -1,53 +1,26 @@
-import java.io.InputStream;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+ 
 public class Sound extends Thread {
-
-    private final String resource;
-
-    public static void play(String resource) {
-        Thread t = new Sound(resource);
-        t.setDaemon(true);
-        t.start();
+    
+    private URL url;
+    private AudioClip sound;
+  
+    public Sound (String musique) {
+        url = this.getClass().getClassLoader().getResource(musique);
+        sound = Applet.newAudioClip(url);
     }
-
-    public Sound(String resource) {
-        this.resource = resource;
+     
+    public void jouer () {
+        sound.play();
     }
-
-    public void run() {
-        Clip clip = null;
-        try {
-            InputStream in = Sound.class.getClassLoader().getResourceAsStream(resource);
-            if(in != null) {
-                AudioInputStream stream = AudioSystem.getAudioInputStream(in);
-                AudioFormat format = stream.getFormat();
-                DataLine.Info info = new DataLine.Info(Clip.class, format);
-                clip = (Clip) AudioSystem.getLine(info);
-                clip.open(stream);
-                clip.loop(0);
-                do  {
-                    try {
-                        Thread.sleep(100);
-                    } catch(InterruptedException iex) {
-                    }
-                } while(clip.isRunning());
-            }
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        } finally {
-            try {
-                if(clip != null) {
-                    clip.close();
-                }
-            } catch(Exception x) {
-                x.printStackTrace(System.out);
-            }
-        }
+     
+    public void jouerEnBoucle () {
+        sound.loop();
+    }
+     
+    public void arreter () {
+        sound.stop();
     }
 }
